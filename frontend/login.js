a6f21b44492170cc878a9ea828ef3484fe2f0874
shinmy321 <36915492+shinmy321@users.noreply.github.com>
2023-07-11 01:49:25 +0900

    로그인유지
    토큰을 이용 로그인을 유지하도록 로컬서버에 저장

--- frontend/login.js
+++ frontend/login.js
@@ -1,7 +1,5 @@
 const form = document.querySelector("#login-form");
 
-let accessToken = null;
-
 const handleSubmit = async (event) => {
   event.preventDefault();
   const formData = new FormData(form);
@@ -13,7 +11,9 @@ const handleSubmit = async (event) => {
     body: formData,
   });
   const data = await res.json();
-  accessToken = data.access_token;
+  const accessToken = data.access_token;
+  window.localStorage.setItem("token", accessToken);
+  alert("로그인 되었습니다");
 
   // if (res.status === 200) {
   //alert("로그인에 성공했습니다");
@@ -22,22 +22,23 @@ const handleSubmit = async (event) => {
   // alert("id 혹은 password가 틀렸습니다");
   //}
 
-  const infoDiv = document.querySelector("#info");
-  infoDiv.innerText = "로그인 되었습니다";
+  // const infoDiv = document.querySelector("#info");
+  // infoDiv.innerText = "로그인 되었습니다";
 
-  const btn = document.createElement("button");
-  btn.innerText = "상품가져오기";
-  btn.addEventListener("click", async () => {
-    const res = await fetch("/items", {
-      //메타데이터를 넣는다
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-    const data = await res.json();
-    console.log(data);
-  });
-  infoDiv.appendChild(btn); // 버튼을 달아준다 infoDiv 에
+  //const btn = document.createElement("button");
+  //btn.innerText = "상품가져오기";
+  //btn.addEventListener("click", async () => {
+  //  const res = await fetch("/items", {
+  //메타데이터를 넣는다
+  //    headers: {
+  //      Authorization: `Bearer ${accessToken}`,
+  //    },
+  //  });
+  //  const data = await res.json();
+  //  console.log(data);
+  //});
+  // infoDiv.appendChild(btn); // 버튼을 달아준다 infoDiv 에
+  window.location.pathname = "/";
 };
 
 form.addEventListener("submit", handleSubmit);
