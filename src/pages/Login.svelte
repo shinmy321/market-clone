4c565fd84648eadd7f58d6fe291b8211f3470bda
shinmy321 <36915492+shinmy321@users.noreply.github.com>
2023-07-12 12:00:47 +0900

    svelte,vite로 바꾼 후 firebase deloy 세

---
+++ src/pages/Login.svelte
@@ -0,0 +1,17 @@
+<form id="login-form" action="/login" method="post">
+  <div>로그인</div>
+  <div>
+    <label for="id">아이디</label>
+    <input type="text" id="id" name="id" required />
+    <!--required 이값이 비어있으면 요청이 안보내짐-->
+  </div>
+  <div>
+    <label for="password">패스워드</label>
+    <input type="password" id="password" name="password" required />
+  </div>
+
+  <div>
+    <button type="submit">로그인</button>
+  </div>
+  <div id="info" />
+</form>
---
+++ src/pages/Signup.svelte
@@ -0,0 +1,28 @@
+<form id="signup-form" action="/signup" method="post">
+  <div>회원가입</div>
+  <div>
+    <label for="id">아이디</label>
+    <input type="text" id="id" name="id" required />
+    <!--required 이값이 비어있으면 요청이 안보내짐-->
+  </div>
+  <div>
+    <label for="password">패스워드</label>
+    <input type="password" id="password" name="password" required />
+  </div>
+  <div>
+    <label for="password2">패스워드 확인</label>
+    <input type="password" id="password2" name="password2" required />
+  </div>
+  <div>
+    <label for="name">이름</label>
+    <input type="text" id="name" name="name" required />
+  </div>
+  <div>
+    <label for="email">이메일</label>
+    <input type="email" id="email" name="email" required />
+  </div>
+  <div>
+    <button type="submit">회원가입하기</button>
+  </div>
+  <div id="info" />
+</form>
